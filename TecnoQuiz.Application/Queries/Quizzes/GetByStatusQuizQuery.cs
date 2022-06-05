ae8e7908f3c95188367184c793a699971482140e
brw.mfatima <brw.mfatima@ifood3rd.com>
2022-06-05 16:43:23 -0300

    query to GetByStatusQuiz

---
+++ TecnoQuiz.Application/Queries/Quizzes/GetByStatusQuizQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TecnoQuiz.Application.ViewModels;
+
+namespace TecnoQuiz.Application.Queries.Quizzes
+{
+    public class GetByStatusQuizQuery : IRequest<IList<QuizViewModel>>
+    {
+        public EQuizStatus status { get; set; }
+    }
+}
---
+++ TecnoQuiz.Application/Queries/Quizzes/GetByStatusQuizQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using TecnoQuiz.Application.ViewModels;
+using TecnoQuiz.Domain.Repositories;
+
+namespace TecnoQuiz.Application.Queries.Quizzes
+{
+    public class GetByStatusQuizQueryHandler : IRequestHandler<GetByStatusQuizQuery, IList<QuizViewModel>>
+    {
+        private readonly IQuizRepository _quizRepository;
+
+        public GetByStatusQuizQueryHandler(IQuizRepository quizRepository)
+        {
+            _quizRepository = quizRepository;
+        }
+        public async Task<IList<QuizViewModel>> Handle(GetByStatusQuizQuery request, CancellationToken cancellationToken)
+        {
+            var quizzes = await _quizRepository.GetByStatusAsync(request.status);
+
+            if (quizzes == null) return null;
+
+            var quizzesViewModel = quizzes.Select(quiz =>
+            new QuizViewModel(
+                quiz.Title,
+                quiz.Description,
+                quiz.Status,
+                new UserViewModel(quiz.User.Id, quiz.User.FullName, quiz.User.Email, quiz.User.Role, quiz.User.Birthday, quiz.User.Document, quiz.User.Active),
+                quiz.Questions.Select(question => new QuestionViewModel(question.Description)).ToList()));
+
+            return quizzesViewModel.ToList();
+        }
+    }
+}
