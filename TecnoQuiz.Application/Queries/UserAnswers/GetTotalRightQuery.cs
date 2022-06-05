4ae1be2ee1601f61c81232ca59bb38ec10c7968a
brw.mfatima <brw.mfatima@ifood3rd.com>
2022-06-05 16:44:07 -0300

    query to GetTotalRight

---
+++ TecnoQuiz.Application/Queries/UserAnswers/GetTotalRightQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+namespace TecnoQuiz.Application.Queries.UserAnswers
+{
+    public class GetTotalRightQuery : IRequest<int>
+    {
+        public Guid UserId { get; set; }
+        public Guid QuizId { get; set; }
+    }
+}
---
+++ TecnoQuiz.Application/Queries/UserAnswers/GetTotalRightQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using TecnoQuiz.Domain.Repositories;
+
+namespace TecnoQuiz.Application.Queries.UserAnswers
+{
+    public class GetTotalRightQueryHandler : IRequestHandler<GetTotalRightQuery, int>
+    {
+        private readonly IUserAnswerRepository _userAnswerRepository;
+
+        public GetTotalRightQueryHandler(IUserAnswerRepository userAnswerRepository)
+        {
+            _userAnswerRepository = userAnswerRepository;
+        }
+
+        public async Task<int> Handle(GetTotalRightQuery request, CancellationToken cancellationToken)
+        {
+            var totalRight = await _userAnswerRepository.GetTotalRight(request.UserId, request.QuizId);
+            return totalRight == null ? 0 : totalRight;
+        }
+    }
+}
