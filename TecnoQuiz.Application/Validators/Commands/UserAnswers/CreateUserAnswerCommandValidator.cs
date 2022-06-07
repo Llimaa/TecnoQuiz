a639ec2f0b78e8c32b501b7e0686940c5fd2dff0
brw.mfatima <brw.mfatima@ifood3rd.com>
2022-06-06 23:59:35 -0300

    adding validators to userAnswer commands

---
+++ TecnoQuiz.Application/Validators/Commands/UserAnswers/CreateUserAnswerCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using TecnoQuiz.Application.Commands.UserAnswers.Inputs;
+
+namespace TecnoQuiz.Application.Validators.Commands.UserAnswers
+{
+    public class CreateUserAnswerCommandValidator : AbstractValidator<CreateUserAnswerCommand>
+    {
+        public CreateUserAnswerCommandValidator()
+        {
+            RuleFor(x => x.AnswerId).NotEmpty()
+                .NotNull()
+                .WithMessage("O campo AnswerId não pode ser valizo ou nulo");
+        }
+    }
+}
---
+++ TecnoQuiz.Application/Validators/Commands/UserAnswers/RemoveUserAnswerCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using TecnoQuiz.Application.Commands.UserAnswers.Inputs;
+
+namespace TecnoQuiz.Application.Validators.Commands.UserAnswers
+{
+    public class RemoveUserAnswerCommandValidator : AbstractValidator<RemoveUserAnswerCommand>
+    {
+        public RemoveUserAnswerCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty()
+                .NotNull()
+                .WithMessage("O campo Id não pode ser vazio ou nulo");
+        }
+    }
+}
