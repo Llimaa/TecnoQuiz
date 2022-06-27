6fb1e307097229a90eef5a7a1785d9a86638b8f0
brw.mfatima <brw.mfatima@ifood3rd.com>
2022-06-27 12:26:53 -0300

    adding Validation Filter

---
+++ TecnoQuiz.API/Filters/ValidationFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace TecnoQuiz.API.Filters
+{
+    public class ValidationFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var message = context.ModelState
+                    .SelectMany(x => x.Value.Errors)
+                    .Select(e => e.ErrorMessage)
+                    .ToList();
+                context.Result = new BadRequestObjectResult(message);
+            }
+        }
+    }
+}
