e3b90de0bc1a937ff9e14ae9791e683077caebf6
brw.mfatima <brw.mfatima@ifood3rd.com>
2022-06-27 12:26:30 -0300

    finish authService

---
+++ TecnoQuiz.Domain/Services/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿
+namespace TecnoQuiz.Domain.Services
+{
+    public interface IAuthService
+    {
+        string GenerateJwtToken(string email, List<string> roles);
+        string ComputeSha256Hash(string password);
+    }
+}
---
+++ TecnoQuiz.Infrastructure/Auth/AuthService.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using TecnoQuiz.Domain.Services;
+
+namespace TecnoQuiz.Infrastructure.Auth
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateJwtToken(string email, List<string> roles)
+        {
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+            var key = _configuration["Jwt:Key"];
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new List<Claim>();
+            claims.Add(new Claim("userName", email));
+            foreach (var item in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, item));
+            }
+
+            var token = new JwtSecurityToken(
+              issuer: issuer,
+              audience: audience,
+              expires: DateTime.Now.AddHours(8),
+              signingCredentials: credentials,
+              claims: claims);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var stringToken = tokenHandler.WriteToken(token);
+
+            return stringToken;
+        }
+
+
+
+        public string ComputeSha256Hash(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder stringBuilder = new StringBuilder();
+
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    stringBuilder.Append(bytes[i].ToString("x2")); // x2 Faz com que seja convertido em prepresentações hexadecimal.
+                }
+
+                return stringBuilder.ToString();
+            }
+        }
+    }
+}
